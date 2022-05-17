628bda45d6dfbda63a8955f207230758bed5ad09
Никита Гурьков <78047666+n-gurkov@users.noreply.github.com>
2022-05-17 12:59:55 +0300

    TMDP API added (#2)

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,3 +1,5 @@
+import { genresListUrl, moviesListUrl, movieUrl } from "./urls";
+
 const users = {
   admin: "12345",
   user: "12345",
@@ -21,4 +23,58 @@ export const checkPassword = (login: string, password: string) => {
     localStorage.setItem("currentUser", login);
     return true;
   }
+};
+
+export const getGenresList = (language: string) => {
+  return fetch(
+    `${genresListUrl}?api_key=${process.env.REACT_APP_API_KEY}&language=${language}`,
+    { method: "GET" }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data.genres;
+    })
+    .catch(() => {
+      return {};
+    });
+};
+
+export const getMoviesList = (
+  page: number,
+  language?: string,
+  withGenres?: string,
+  year?: number,
+  rating?: number
+) => {
+  return fetch(
+    `${moviesListUrl}?api_key=${process.env.REACT_APP_API_KEY}&language=${language}&with_genres=${withGenres}&year=${year}&vote_average.gte=${rating}&page=${page}`,
+    { method: "GET" }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch(() => {
+      return {};
+    });
+};
+
+export const getMovie = (movieId: number, language?: string) => {
+  return fetch(
+    `${movieUrl}${movieId}?api_key=${process.env.REACT_APP_API_KEY}&language=${language}`,
+    { method: "GET" }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch(() => {
+      return {};
+    });
 };
