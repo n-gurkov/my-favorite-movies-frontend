31a8d1fd0414ec482709238c78d99dbdffe7f5ae
Никита Гурьков <gurkov.nikita@gmail.com>
2022-05-17 11:10:57 +0300

    TMDP API added

---
+++ src/urls.tsx
@@ -0,0 +1,3 @@
+export const genresListUrl = "https://api.themoviedb.org/3/genre/movie/list";
+export const moviesListUrl = "https://api.themoviedb.org/3/discover/movie/";
+export const movieUrl = "https://api.themoviedb.org/3/movie/";
